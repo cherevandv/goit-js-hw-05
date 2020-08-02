e4ce653a9e5644531dc1a8381c187cd1e4938c2b
Daria Kobekhno <cherevandv@gmail.com>
2020-08-02 18:37:21 +0300

    added tasks 1-4

---
+++ js/task-03.js
@@ -0,0 +1,36 @@
+'use strict';
+
+class Storage{
+ constructor(arg){
+     this.items = [...arg];
+ }
+ getItems(){
+     return this.items;
+ }
+ addItem(item){
+    this.items.push(item);
+ }
+ removeItem(item){
+     if(this.items.includes(item)){
+         let i = this.items.indexOf(item);
+         this.items.splice(i,1);
+     }
+ }
+}
+
+
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+  ]);
+  
+  const items = storage.getItems();
+  console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  
+  storage.addItem('Дроид');
+  console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+  console.log(storage.removeItem('Пролонгер'));
+  console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
